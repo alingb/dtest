274c70cc93d54b5671e15f2bd0e9a93107573802
ytym00 <375272990@qq.com>
2018-08-05 13:43:39 +0800

    test

---
+++ web/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(req):
+    return render(req, 'test.html')
+
+
+def base(req):
+    return render(req, 'base.html')
