1936b2ff62f82dd3a1df9e6c8ce41d4a6d9854c1
master <375272990@qq.com>
2018-08-22 10:28:41 +0800

    update 2018-08-22

---
+++ web/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.base, name="disk_index"),
+    url(r'get_info/', views.get_info, name="web_get_info"),
+]
